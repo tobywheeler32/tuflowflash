8bce564ef0e878bdff73ee5fdfc823ee230466c7
lokhorstivar <i.r.lokhorst@gmail.com>
2022-04-07 15:54:02 +0200

    console config

--- setup.py
+++ setup.py
@@ -11,10 +11,11 @@ setup(
     # Needed for dependencies
     install_requires=["gdal","argparse","configparser","typing","cftime","netCDF4","pandas",],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.2',
     # The license can be anything you like
     license='MIT',
     description='An example of a python package from pre-existing code',
+    entry_points={"console_scripts": ["run-tuflow-flash = tuflowflash.start_sim:main"]}
     # We will also need a readme eventually (there will be a warning)
     # long_description=open('README.txt').read(),
 )
