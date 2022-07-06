5a2c44fbaf5a49445c50dba0d5da4bfb9d349359
lokhorstivar <103189202+lokhorstivar@users.noreply.github.com>
2022-07-06 11:36:17 +0200

    Update prepare_data.py

--- tuflowflash/prepare_data.py
+++ tuflowflash/prepare_data.py
@@ -161,10 +161,14 @@ class prepareData:
 
     def process_rainfall_timeseries_for_tuflow(self, rain_df,utc_reference_time):
         rain_df["time"] = 0.0
+        rain_df['datetime'] = rain_df.index
         for x in range(len(rain_df)):
             rain_df["time"][x] = (
                 rain_df.index[x] - utc_reference_time
             ).seconds / 3600
+            print(rain_df.index[x])
+            print(utc_reference_time)
+            print(rain_df["time"][x])
         rain_df.set_index("time", inplace=True)
         for col in rain_df.columns:
             rain_df[col].values[0] = 0
