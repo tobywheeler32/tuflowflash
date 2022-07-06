58adf1c4cab7d0d0ce5cdf524fe9eb0c2f00120f
lokhorstivar <103189202+lokhorstivar@users.noreply.github.com>
2022-07-06 15:20:41 +0200

    Update prepare_data.py

--- tuflowflash/prepare_data.py
+++ tuflowflash/prepare_data.py
@@ -51,6 +51,7 @@ class prepareData:
         local_reference_time = local.localize(self.settings.reference_time, is_dst=None)
         utc_reference_time = local_reference_time.astimezone(pytz.utc)
         rain_df = self.process_rainfall_timeseries_for_tuflow(rain_df,utc_reference_time)
+        print(rain_df)
         rain_df.to_csv(self.settings.gauge_rainfall_file)
         logger.info("succesfully written rainfall file")
 
